4917a2b94d6649d3c00bfed4fe88b94e50ab79e9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-05-24 12:13:30 +0900

    .

--- carrotlib/js/protect.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementsByTagName('img').forEach(function (image) {
-    if (image.parentNode.tagName.toLowerCase() != 'a') {
-      image.style.pointerEvents = 'none';
-    }
-  });
-});
